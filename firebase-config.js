5293f0350ee73d437530eab82ffd37de78a66483
Shehla Mushtaq <shehla.asim.78@gmail.com>
2021-01-07 02:38:21 +0500

    Merge branch 'master' of https://github.com/shehlamushtaq/backend-work into master

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// var firebase = require("firebase/app");
+// require("firebase/auth");
+
+// // Add the Firebase products that you want to use
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDkcRFlR3o7NpdnorMc7F2RzN3ZsWUFb3E",
+//   authDomain: "login-d1a45.firebaseapp.com",
+//   databaseURL: "https://login-d1a45-default-rtdb.firebaseio.com",
+//   projectId: "login-d1a45",
+//   storageBucket: "login-d1a45.appspot.com",
+//   messagingSenderId: "333003973286",
+//   appId: "1:333003973286:web:4015ecf84f5d0fa6e70a67",
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// module.exports = firebase;
--- index.js
+++ index.js
@@ -3,66 +3,104 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
+
+// const firebase = require("./firebase-config");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
-
-const users = [{name:'shehla',email:'shehla@dd.com',password:'123654',id:1 }];
-
 app.listen(PORT, (req, res) => {
   console.log("My first Server is running at port: ", PORT);
 });
+
+const users = [
+  { name: "shehla", email: "shehla@dd.com", password: "123654", id: 1 },
+];
 
 app.use(express.static(path.join(__dirname, "public")));
 
 //===============================================================
 
 app.get("/signup", (req, res) => {
-    res.sendFile(path.join(__dirname, "registration", "signup2.html"));
+  console.log("path", path.join(__dirname));
+  res.sendFile(path.join(__dirname, "registration", "signup2.html"));
 });
-app.post("/signup", (req, res) => {
-    console.log(req.body);
-    let {field1, field3,field5} = req.body
-    let found = users.some((item)=>item.email == field3)
-    if(found){
-        res.send('<h1>user already exists, try another</h1>')
-    }else{
-    users.push({ name:field1, email:field3, password:field5, id:users.length+1});
 
-    res.redirect('/signin')
-}
-})
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  let { field1, field3, field5 } = req.body;
+
+  let found = users.some((item) => item.email == field3);
+  if (found) {
+    res.send("<h1>user already exists, try another</h1>");
+  } else {
+    users.push({
+      name: field1,
+      email: field3,
+      password: field5,
+      id: users.length + 1,
+    });
+    res.redirect("/signin");
+  }
+});
+
 //================================================================================
 
 app.get("/signin", (req, res) => {
-    res.sendFile(path.join(__dirname, "registration", "signin.html"));
+  res.sendFile(path.join(__dirname, "registration", "signin.html"));
 });
+
 app.post("/signin", (req, res) => {
-    let {field3,field5} = req.body
-    let found = users.some((item)=>item.email==field3 && item.password==field5 )
-    if(found){
-        res.send('<h1>Welcome You are Logged in</h1>')
-    }else{
-        // res.send('<h1>password not matched</h1>')
-        res.redirect('/signin')
-    }
-})
+  let { field3, field5 } = req.body;
+
+  let found = users.some(
+    (item) => item.email == field3 && item.password == field5
+  );
+  if (found) {
+    //res.send("<h1>Welcome You are Logged in</h1>");
+    res.sendFile(path.join(__dirname, "registration", "signin_success.html"));
+  } else {
+    // res.send('<h1>password not matched</h1>')
+    res.redirect("/signin");
+  }
+});
+
+//=====================================================================================================
+//   firebase
+//     .auth()
+//     .createUserWithEmailAndPassword(field3, field5)
+//     .then((user) => {
+//       console.log("saved to firebase");
+
+//       res.redirect("/signin");
+//     })
+//     .catch((error) => {
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       console.log(error);
+//       res.send("<h1>user already exists, try another</h1>");
+//     });
+// });
+//====================================================================================================
+//   firebase
+//     .auth()
+//     .signInWithEmailAndPassword(field3, field5)
+//     .then((user) => {
+//       console.log(user);
+//       res.sendFile(path.join(__dirname, "registration", "signin_success.html"));
+//     })
+//     .catch((error) => {
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       res.redirect("/signin");
+//     });
+// });
 
 //=====================================================================
-// 
+//
 
 // app.get("/all-users", (req, res) => {
 //     res.send(users);
 // });
-
-
-
-
-
-
-
-
-
-
 
 // app.get('/', (req, res) => {
 //     res.send("<h1>My First  Home Page</h1>")
@@ -84,3 +122,4 @@ app.post("/signin", (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'contact.html'))
 // })
 //https://www.sanwebe.com/2014/08/css-html-forms-designs
+//===================================================================
